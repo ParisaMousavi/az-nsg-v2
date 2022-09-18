a2e36c6d378a93e8b262e143da89195436f93fde
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-09-18 19:01:26 +0200

    update

--- main.tf
+++ main.tf
@@ -20,11 +20,11 @@ resource "azurerm_network_security_rule" "this" {
   access                       = each.value.access
   protocol                     = each.value.protocol
   source_port_range            = each.value.source_port_range
-  source_port_ranges           = each.value.source_port_ranges
+  source_port_ranges           = each.value.source_port_ranges == [] ? null : each.value.source_port_ranges
   destination_port_range       = each.value.destination_port_range
-  destination_port_ranges      = each.value.destination_port_ranges
+  destination_port_ranges      = each.value.destination_port_ranges == [] ? null : each.value.destination_port_ranges
   source_address_prefix        = each.value.source_address_prefix
-  source_address_prefixes      = each.value.source_address_prefixes
+  source_address_prefixes      = each.value.source_address_prefixes == [] ? null : each.value.source_address_prefixes
   destination_address_prefix   = each.value.destination_address_prefix
-  destination_address_prefixes = each.value.destination_address_prefixes
+  destination_address_prefixes = each.value.destination_address_prefixes == [] ? null : each.value.destination_address_prefixes
 }
