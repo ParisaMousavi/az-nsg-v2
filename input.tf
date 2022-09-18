ca4a64a28a866438b9de20f6c3dfd79a925cbeb6
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-09-18 18:57:36 +0200

    update

--- input.tf
+++ input.tf
@@ -23,13 +23,13 @@ variable "security_rules" {
     access                       = string
     protocol                     = string
     source_port_range            = string
-    source_port_ranges           = string
+    source_port_ranges           = list(string)
     destination_port_range       = string
-    destination_port_ranges      = string
+    destination_port_ranges      = list(string)
     source_address_prefix        = string
-    source_address_prefixes      = string
+    source_address_prefixes      = list(string)
     destination_address_prefix   = string
-    destination_address_prefixes = string
+    destination_address_prefixes = list(string)
     description                  = string
   }))
   default = null
