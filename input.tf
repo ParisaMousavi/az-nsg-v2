f628fa01e4d44e104db1a48d2f81ff2f3fae5520
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-09-18 18:23:01 +0200

    update

--- input.tf
+++ input.tf
@@ -16,16 +16,21 @@ variable "additional_tags" {
 }
 
 variable "security_rules" {
-    type =  list(object({
-    name                       = string
-    priority                   = number
-    direction                  = string
-    access                     = string
-    protocol                   = string
-    source_port_range          = string
-    destination_port_range     = string
-    source_address_prefix      = string
-    destination_address_prefix = string
-    }))
+  type = list(object({
+    name                         = string
+    priority                     = number
+    direction                    = string
+    access                       = string
+    protocol                     = string
+    source_port_range            = string
+    source_port_ranges           = string
+    destination_port_range       = string
+    destination_port_ranges      = string
+    source_address_prefix        = string
+    source_address_prefixes      = string
+    destination_address_prefix   = string
+    destination_address_prefixes = string
+    description                  = string
+  }))
   default = null
 }
--- main.tf
+++ main.tf
@@ -10,21 +10,25 @@ resource "azurerm_network_security_group" "this" {
   )
 }
 
-# resource "azurerm_network_security_rule" "this" {
-#   for_each                     = local.nsg_rules_by_key
-#   name                         = each.value.name
-#   resource_group_name          = var.resource_group_name
-#   network_security_group_name  = azurerm_network_security_group.this.name
-#   priority                     = each.value.priority
-#   direction                    = each.value.direction
-#   access                       = each.value.access
-#   protocol                     = each.value.protocol
-#   source_port_range            = lookup(each.value, "source_port_range", null)
-#   source_port_ranges           = lookup(each.value, "source_port_ranges", null)
-#   destination_port_range       = lookup(each.value, "destination_port_range", null)
-#   destination_port_ranges      = lookup(each.value, "destination_port_ranges", null)
-#   source_address_prefix        = lookup(each.value, "source_address_prefix", null)
-#   source_address_prefixes      = lookup(each.value, "source_address_prefixes", null)
-#   destination_address_prefix   = lookup(each.value, "destination_address_prefix", null)
-#   destination_address_prefixes = lookup(each.value, "destination_address_prefixes", null)
-# }
+resource "azurerm_network_security_rule" "this" {
+  for_each                     = var.security_rules
+  name                         = each.value.name
+  resource_group_name          = var.resource_group_name
+  network_security_group_name  = azurerm_network_security_group.this.name
+  priority                     = each.value.priority
+  direction                    = each.value.direction
+  access                       = each.value.access
+  protocol                     = each.value.protocol
+
+  source_port_range            = each.value.protocol
+  source_port_ranges           = each.value.protocol
+
+  destination_port_range       = each.value.protocol
+  destination_port_ranges      = each.value.protocol
+
+  source_address_prefix        = each.value.protocol
+  source_address_prefixes      = each.value.protocol
+  
+  destination_address_prefix   = each.value.protocol
+  destination_address_prefixes = each.value.protocol
+}
