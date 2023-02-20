2a62a9e0b17d2ffce693893ae261c304dc6c413f
Parisa Moosavinezhad <parisamoosavinezhad@yahoo.com>
2023-02-20 09:30:26 +0100

    update

--- input.tf
+++ input.tf
@@ -17,24 +17,21 @@ variable "additional_tags" {
 
 variable "security_rules" {
   type = list(object({
-    name                         = string
-    priority                     = number
-    direction                    = string
-    access                       = string
-    protocol                     = string
-    source_port_range            = string
-    source_port_ranges           = list(string)
-    destination_port_range       = string
-    destination_port_ranges      = list(string)
-    source_address_prefix        = string
-    source_address_prefixes      = list(string)
-    destination_address_prefix   = string
-    destination_address_prefixes = list(string)
-    description                  = string
+    name              = string
+    priority          = number
+    direction         = string
+    access            = string
+    protocol          = string
+    source_port_range = string
+    # source_port_ranges           = list(string)
+    destination_port_range = string
+    # destination_port_ranges      = list(string)
+    source_address_prefix = string
+    # source_address_prefixes      = list(string)
+    destination_address_prefix = string
+    # destination_address_prefixes = list(string)
+    description = string
   }))
-  default = null
+  default = []
 }
 
-variable "subnet_id" {
-  type = string 
-}
--- main.tf
+++ main.tf
@@ -21,16 +21,11 @@ resource "azurerm_network_security_rule" "this" {
   protocol                    = each.value.protocol
   source_port_range           = each.value.source_port_range
   destination_port_range      = each.value.destination_port_range
-  source_address_prefix      = each.value.source_address_prefix
-  destination_address_prefix = each.value.destination_address_prefix
+  source_address_prefix       = each.value.source_address_prefix
+  destination_address_prefix  = each.value.destination_address_prefix
   # source_port_ranges           = each.value.source_port_ranges 
   # destination_port_ranges      = each.value.destination_port_ranges 
   # source_address_prefixes      = each.value.source_address_prefixes 
   # destination_address_prefixes = each.value.destination_address_prefixe 
 }
 
-resource "azurerm_subnet_network_security_group_association" "this" {
-  count                     = var.subnet_id == null ? 0 : 1
-  subnet_id                 = var.subnet_id
-  network_security_group_id = azurerm_network_security_group.this.id
-}
