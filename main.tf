3ff11bd601b199eab3857b46d14c835242577803
Parisa Moosavinezhad <parisamoosavinezhad@yahoo.com>
2023-02-21 23:54:43 +0100

    update

--- main.tf
+++ main.tf
@@ -19,13 +19,13 @@ resource "azurerm_network_security_rule" "this" {
   direction                    = each.value.direction
   access                       = each.value.access
   protocol                     = each.value.protocol
-  source_port_range            = length(each.value.source_port_ranges) == 0 ? each.value.source_port_range : null
-  source_port_ranges           = length(each.value.source_port_ranges) == 0 ? null : each.value.source_port_ranges
-  destination_port_range       = length(each.value.destination_port_ranges) == 0 ? each.value.destination_port_range : null
-  destination_port_ranges      = length(each.value.destination_port_ranges) == 0 ? null : each.value.destination_port_ranges
-  source_address_prefix        = length(each.value.source_address_prefixes) == 0 ? each.value.source_address_prefix : null
-  source_address_prefixes      = length(each.value.source_address_prefixes) == 0 ? null : each.value.source_address_prefixes
-  destination_address_prefix   = length(each.value.destination_address_prefixes) == 0 ? each.value.destination_address_prefix : null
-  destination_address_prefixes = length(each.value.destination_address_prefixes) == 0 ? null : each.value.destination_address_prefixes
+  source_port_range            = each.value.source_port_range
+  source_port_ranges           = each.value.source_port_ranges
+  destination_port_range       = each.value.destination_port_range
+  destination_port_ranges      = each.value.destination_port_ranges
+  source_address_prefix        = each.value.source_address_prefix
+  source_address_prefixes      = each.value.source_address_prefixes
+  destination_address_prefix   = each.value.destination_address_prefix 
+  destination_address_prefixes = each.value.destination_address_prefixes
 }
 
